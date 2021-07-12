24b3f4a451aad4eb015997e2c8f2bfee47868bd0
Xiangyi Meng <xymeng16@gmail.com>
2021-07-12 19:23:56 +0800

    Error handling, generic types, traits, lifetimes, tests and minigrep.

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,113 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // () is the unit type, meaning return nothing
+    // Box<dyn Error> means the function will return a type that implements the Error trait
+    let contents = fs::read_to_string(config.filename)?; // ? will return the error value
+                                                         // from the current function for the caller to handle
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+    for line in results {
+        println!("{}", line);
+    }
+    return Ok(());
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(args: &Vec<String>) -> Result<Config, &str> {
+        // constructor
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err(); // if CASE_INSENSITICE is not set, return true
+        return Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        });
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    // since the returned &str is borrowed from contents, they should have the same lifetime
+    /* Ref from https://doc.rust-lang.org/book/ch12-04-testing-the-librarys-functionality.html
+    In other words, we tell Rust that the data returned by the search function will live as long
+    as the data passed into the search function in the contents argument. This is important!
+    The data referenced by a slice needs to be valid for the reference to be valid; if the compiler
+    assumes we’re making string slices of query rather than contents, it will do its safety checking incorrectly.
+     */
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    return results;
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    // since the returned &str is borrowed from contents, they should have the same lifetime
+    /* Ref from https://doc.rust-lang.org/book/ch12-04-testing-the-librarys-functionality.html
+    In other words, we tell Rust that the data returned by the search function will live as long
+    as the data passed into the search function in the contents argument. This is important!
+    The data referenced by a slice needs to be valid for the reference to be valid; if the compiler
+    assumes we’re making string slices of query rather than contents, it will do its safety checking incorrectly.
+     */
+    let query = query.to_lowercase(); // old &str query is moved
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    return results;
+}
